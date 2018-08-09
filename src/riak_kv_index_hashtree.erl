6cdd2effaab850a3af9281028b71f47a20e7ae64
Nordine Saadouni <saadouni85@gmail.com>
2018-08-09 10:23:53 +0100

    fixed update/3 with undefined callback

--- src/riak_kv_index_hashtree.erl
+++ src/riak_kv_index_hashtree.erl
@@ -162,6 +162,8 @@ update(Id, Tree) ->
 
 %% @doc Update all hashtrees managed by the provided index_hashtree pid.
 -spec update(index_n(), pid(),  undefined | update_callback()) -> ok | not_responsible.
+update(Id, Tree, undefiend) ->
+    gen_server:call(Tree, {update_tree, Id, undefiend}, infinity);
 update(Id, Tree, Callback) when is_function(Callback) ->
     gen_server:call(Tree, {update_tree, Id, Callback}, infinity).
 
